a7dfc6581250601c8b31d53b40c6d44a16c7c6d7
philip_lai <pupuhouse@yahoo.com.tw>
2019-11-30 12:29:31 +0800

    add mongoose connection and create record model and user model

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-const express = require('express')               // 載入 express
-const app = express()                            // 建立 express instance
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+// 加上 { useNewUrlParser: true }
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/moneybook', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+const Record = require('./models/record')
+const User = require('./models/user')
 
 // 設定第一個首頁路由
 app.get('/', (req, res) => {
