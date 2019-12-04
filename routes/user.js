b879d2248b1e692a4bbf00d14ece0f9cc218aa1e
philip_lai <pupuhouse@yahoo.com.tw>
2019-12-04 15:24:30 +0800

    add bcrypt to user registtration route

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
-// const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs')
 const User = require('../models/user')
 
 // 登入頁面
@@ -42,11 +42,17 @@ router.post('/register', (req, res) => {
         email,
         password
       })
-      newUser
-        .save()
-        .then(user => { res.redirect('/') })
-        .catch(err => console.log(err))
 
+      bcrypt.genSalt(10, (err, salt) =>
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err
+          newUser.password = hash
+          newUser
+            .save()
+            .then(user => { res.redirect('/') })
+            .catch(err => console.log(err))
+        })
+      )
     }
   })
   //res.send('register')
